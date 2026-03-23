e63b0bb48961c7a2be04f0e1111095fbe318c715
Andrew Kress <andrew.kress@gmail.com>
2026-03-22 19:20:32 -0500

    add more fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_21_141206) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_23_000345) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -50,6 +50,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_21_141206) do
   end
 
   create_table "resumes", force: :cascade do |t|
+    t.string "application_link"
+    t.string "company_name"
     t.datetime "created_at", null: false
     t.text "job_description"
     t.string "original_filename"
